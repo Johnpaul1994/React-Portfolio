92209a0eea23161b6a5e243ab3b03d6598a11da2
Johnpaul1994 <johnpaulnedolisa@gmail.com>
2023-07-30 22:22:44 +0100

    new commit

---
+++ src/components/HeroImg2.js
@@ -0,0 +1,18 @@
+import "./HeroImgStyles2.css"
+ R
+
+import React from 'react'
+
+const heroImg2 = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>Projects.</h1>
+        <p>Some of my recent works</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default heroImg2
